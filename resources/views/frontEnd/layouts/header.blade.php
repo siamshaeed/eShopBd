d6eb87b331f426ea67fed63b383a0fcd24c79dc9
siamshaeed <siamshaeed@gmail.com>
2021-03-11 21:08:06 -0800

    logo and top header modify

--- resources/views/frontEnd/layouts/header.blade.php
+++ resources/views/frontEnd/layouts/header.blade.php
@@ -30,7 +30,7 @@
             <div class="row">
                 <div class="col-sm-4">
                     <div class="logo pull-left">
-                        <a href="{{url('/')}}"><img src="{{asset('frontEnd/images/home/logo.png')}}" alt="" /></a>
+                        <a href="{{url('/')}}"><img src="{{asset('frontEnd/images/home/logo.jpg')}}" alt="" /></a>
                     </div>
                     <div class="btn-group pull-right">
                         <div class="btn-group">
