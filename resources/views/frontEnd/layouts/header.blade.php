a60d2be68e391da89d88d0b3d0f28c65ec46d32d
siamshaeed <siamshaeed@gmail.com>
2021-03-11 21:31:04 -0800

    socail media add

--- resources/views/frontEnd/layouts/header.blade.php
+++ resources/views/frontEnd/layouts/header.blade.php
@@ -11,13 +11,12 @@
                     </div>
                 </div>
                 <div class="col-sm-6">
-                    <div class="social-icons pull-right">
+                    <div class="social-icons myclass pull-right">
                         <ul class="nav navbar-nav">
-                            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                            <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-                            <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
-                            <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                            <li><a href="https://www.facebook.com/siamshaeed/" target="_blank"><i class="fa fa-facebook"></i></a></li>
+                            <li><a href="https://twitter.com/siamshaeed"><i class="fa fa-twitter" target="_blank"></i></a></li>
+                            <li><a href="https://www.linkedin.com/in/siamshaeed/"><i class="fa fa-linkedin" target="_blank"></i></a></li>
+                            <li><a href="https://github.com/siamshaeed"><i class="fa fa-github" target="_blank"></i></a></li>
                         </ul>
                     </div>
                 </div>
