1a206b9337a4f7c8fdb17c4cf8aa912df6b74796
siamshaeed <siamshaeed@gmail.com>
2021-03-15 21:51:20 -0700

    line raindent

--- resources/views/backEnd/layouts/footer.blade.php
+++ resources/views/backEnd/layouts/footer.blade.php
@@ -1,3 +1,3 @@
 <div class="row-fluid">
-    <div id="footer" class="span12">Copyright ©2021 Laravel5.7. All rights reserved.</div>
+	<div id="footer" class="span12">Copyright ©2021 Laravel5.7. All rights reserved.</div>
 </div>
